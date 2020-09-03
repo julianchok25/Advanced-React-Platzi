71dc6297ee0252d87f57817ea2f816571c88d444
Julian Villegas <julian.villegasplus@gmail.com>
2020-09-02 21:30:13 -0500

    [ADD]index.html: Adding Babel to manipulate JSX and create an html to include a div and compile with webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Iniciando");
+import React from "react";
+import ReacDOM from "react-dom";
+
+const app = document.getElementById("app");
+ReacDOM.render(<h1>Hola, probando...</h1>, app);
